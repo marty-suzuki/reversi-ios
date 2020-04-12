4e2a8927c363b3daf77189722881fa40bfb1cc01
marty-suzuki <s1180183@gmail.com>
2020-04-13 00:29:13 +0900

    Add ReversiLogic.framework

---
+++ ReversiLogic/ReversiLogic.h
@@ -0,0 +1,19 @@
+//
+//  ReversiLogic.h
+//  ReversiLogic
+//
+//  Created by marty-suzuki on 2020/04/13.
+//  Copyright © 2020 Yuta Koshizawa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReversiLogic.
+FOUNDATION_EXPORT double ReversiLogicVersionNumber;
+
+//! Project version string for ReversiLogic.
+FOUNDATION_EXPORT const unsigned char ReversiLogicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReversiLogic/PublicHeader.h>
+
+
