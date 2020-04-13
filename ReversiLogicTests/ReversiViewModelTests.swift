f55d8a25682ecfcb22781c7f0cec12e4deadc0f5
marty-suzuki <s1180183@gmail.com>
2020-04-14 02:42:08 +0900

    Fix ReversiViewModelTests to use MockResponse

--- ReversiLogicTests/ReversiViewModelTests.swift
+++ ReversiLogicTests/ReversiViewModelTests.swift
@@ -14,13 +14,15 @@ final class ReversiViewModelTests: XCTestCase {
         let turn = Disk.dark
 
         viewModel.turn = turn
-        dependency.selectedSegmentIndexForResponse = 0
+        dependency._selectedSegmentIndexFor = 0
 
         viewModel.waitForPlayer()
 
-        XCTAssertEqual(dependency.selectedSegmentIndexForCalledCount, 1)
-        XCTAssertEqual(dependency.selectedSegmentIndexForParameters, [turn.index])
-        XCTAssertEqual(dependency.playTurnOfComputerCalledCount, 0)
+        let selectedSegmentIndexFor = dependency.$_selectedSegmentIndexFor
+        let playTurnOfComputer = dependency.$_playTurnOfComputer
+        XCTAssertEqual(selectedSegmentIndexFor.calledCount, 1)
+        XCTAssertEqual(selectedSegmentIndexFor.parameters, [turn.index])
+        XCTAssertEqual(playTurnOfComputer.calledCount, 0)
     }
 
     func test_waitForPlayer_turnがlightで_selectedSegmentIndexが1の場合() {
@@ -28,13 +30,15 @@ final class ReversiViewModelTests: XCTestCase {
         let turn = Disk.light
 
         viewModel.turn = turn
-        dependency.selectedSegmentIndexForResponse = 1
+        dependency._selectedSegmentIndexFor = 1
 
         viewModel.waitForPlayer()
 
-        XCTAssertEqual(dependency.selectedSegmentIndexForCalledCount, 1)
-        XCTAssertEqual(dependency.selectedSegmentIndexForParameters, [turn.index])
-        XCTAssertEqual(dependency.playTurnOfComputerCalledCount, 1)
+        let selectedSegmentIndexFor = dependency.$_selectedSegmentIndexFor
+        let playTurnOfComputer = dependency.$_playTurnOfComputer
+        XCTAssertEqual(selectedSegmentIndexFor.calledCount, 1)
+        XCTAssertEqual(selectedSegmentIndexFor.parameters, [turn.index])
+        XCTAssertEqual(playTurnOfComputer.calledCount, 1)
     }
 
     func test_waitForPlayer_turnがnilの場合() {
@@ -43,33 +47,35 @@ final class ReversiViewModelTests: XCTestCase {
         viewModel.turn = nil
         viewModel.waitForPlayer()
 
-        XCTAssertEqual(dependency.selectedSegmentIndexForCalledCount, 0)
-        XCTAssertEqual(dependency.selectedSegmentIndexForParameters, [])
-        XCTAssertEqual(dependency.playTurnOfComputerCalledCount, 0)
+        let selectedSegmentIndexFor = dependency.$_selectedSegmentIndexFor
+        let playTurnOfComputer = dependency.$_playTurnOfComputer
+        XCTAssertEqual(selectedSegmentIndexFor.calledCount, 0)
+        XCTAssertEqual(selectedSegmentIndexFor.parameters, [])
+        XCTAssertEqual(playTurnOfComputer.calledCount, 0)
     }
 }
 
 extension ReversiViewModelTests {
 
     final class Dependency {
-        private(set) var playTurnOfComputerCalledCount = 0
 
-        private(set) var selectedSegmentIndexForCalledCount = 0
-        private(set) var selectedSegmentIndexForParameters: [Int] = []
-        var selectedSegmentIndexForResponse = 0
+        @MockResponse<Void, Void>()
+        var _playTurnOfComputer: Void
+
+        @MockResponse<Int, Int>
+        var _selectedSegmentIndexFor = 0
 
         private(set) lazy var testTarget = ReversiViewModel(
             playTurnOfComputer: { [weak self] in self?.playTurnOfComputer() },
-            selectedSegmentIndexFor: { [weak self] in self?.selectedSegmentIndexFor($0) })
+            selectedSegmentIndexFor: { [weak self] in self?.selectedSegmentIndexFor($0) }
+        )
 
         func playTurnOfComputer() {
-            playTurnOfComputerCalledCount += 1
+            __playTurnOfComputer.respond()
         }
 
         func selectedSegmentIndexFor(_ index: Int) -> Int {
-            selectedSegmentIndexForCalledCount += 1
-            selectedSegmentIndexForParameters += [index]
-            return selectedSegmentIndexForResponse
+            return __selectedSegmentIndexFor.respond(index)
         }
     }
 }
