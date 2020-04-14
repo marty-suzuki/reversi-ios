41970f48ba739d1bfcba7893bc976dfe592e3278
marty-suzuki <s1180183@gmail.com>
2020-04-15 01:11:47 +0900

    Add loadGame test case to ReversiViewModelTests

--- ReversiLogicTests/ReversiViewModelTests.swift
+++ ReversiLogicTests/ReversiViewModelTests.swift
@@ -69,6 +69,48 @@ final class ReversiViewModelTests: XCTestCase {
         XCTAssertEqual(selectedSegmentIndexFor.calledCount, 1)
         XCTAssertEqual(selectedSegmentIndexFor.parameters, [turn.index])
     }
+
+    func test_loadGame() throws {
+        let viewModel = dependency.testTarget
+
+        let expectedCell = GameData.Board.Cell(x: 0, y: 0, disk: nil)
+        let expectedPlayerDark = GameData.Player.computer
+        let expectedPlayerLight = GameData.Player.computer
+
+        dependency.gameData = GameData(
+            status: .turn(.dark),
+            playerDark: expectedPlayerDark,
+            playerLight: expectedPlayerLight,
+            board: GameData.Board(cells: [[expectedCell]])
+        )
+
+        try viewModel.loadGame()
+
+        XCTAssertEqual(viewModel.turn, .dark)
+
+        let setPlayerDarkSelectedIndex = dependency.$setPlayerDarkSelectedIndex
+        XCTAssertEqual(setPlayerDarkSelectedIndex.calledCount, 1)
+        XCTAssertEqual(setPlayerDarkSelectedIndex.parameters, [expectedPlayerDark.rawValue])
+
+        let setPlayerLightSelectedIndex = dependency.$setPlayerLightSelectedIndex
+        XCTAssertEqual(setPlayerLightSelectedIndex.calledCount, 1)
+        XCTAssertEqual(setPlayerLightSelectedIndex.parameters, [expectedPlayerLight.rawValue])
+
+        let setDisk = dependency.$setDisk
+        XCTAssertEqual(setDisk.calledCount, 1)
+
+        let parameter = try XCTUnwrap(setDisk.parameters.first)
+        XCTAssertEqual(parameter.0, expectedCell.disk)
+        XCTAssertEqual(parameter.1, expectedCell.x)
+        XCTAssertEqual(parameter.2, expectedCell.y)
+        XCTAssertEqual(parameter.3, false)
+
+        let updateMessageViews = dependency.$updateMessageViews
+        XCTAssertEqual(updateMessageViews.calledCount, 1)
+
+        let updateCountLabels = dependency.$updateCountLabels
+        XCTAssertEqual(updateCountLabels.calledCount, 1)
+    }
 }
 
 extension ReversiViewModelTests {
@@ -81,9 +123,32 @@ extension ReversiViewModelTests {
         @MockResponse<Int, Int>
         var _selectedSegmentIndexFor = 0
 
+        @MockResponse<(Disk?, Int, Int, Bool), Void>()
+        var setDisk: Void
+
+        @MockResponse<Int, Void>()
+        var setPlayerDarkSelectedIndex: Void
+
+        @MockResponse<Int, Void>()
+        var setPlayerLightSelectedIndex: Void
+
+        @MockResponse<Void, Void>()
+        var updateCountLabels: Void
+
+        @MockResponse<Void, Void>()
+        var updateMessageViews: Void
+
+        var gameData = Const.initialData
+
         private(set) lazy var testTarget = ReversiViewModel(
             playTurnOfComputer: { [weak self] in self?.playTurnOfComputer() },
-            selectedSegmentIndexFor: { [weak self] in self?.selectedSegmentIndexFor($0) }
+            selectedSegmentIndexFor: { [weak self] in self?.selectedSegmentIndexFor($0) },
+            setDisk: { [weak self] disk, x, y, animated, _ in self?._setDisk.respond((disk, x, y, animated)) },
+            setPlayerDarkSelectedIndex: { [weak self] in self?._setPlayerDarkSelectedIndex.respond($0) },
+            setPlayerLightSelectedIndex: { [weak self] in self?._setPlayerLightSelectedIndex.respond($0) },
+            updateCountLabels: { [weak self] in self?._updateCountLabels.respond() },
+            updateMessageViews: { [weak self] in self?._updateMessageViews.respond() },
+            loadGame: { [weak self] _, completion in completion(self?.gameData ?? Const.initialData) }
         )
 
         func playTurnOfComputer() {
@@ -93,5 +158,17 @@ extension ReversiViewModelTests {
         func selectedSegmentIndexFor(_ index: Int) -> Int {
             return __selectedSegmentIndexFor.respond(index)
         }
+    }
+}
+
+extension ReversiViewModelTests.Dependency {
+
+    private enum Const {
+        static let initialData = GameData(
+            status: .turn(.dark),
+            playerDark: .manual,
+            playerLight: .manual,
+            board: .initial()
+        )
     }
 }
